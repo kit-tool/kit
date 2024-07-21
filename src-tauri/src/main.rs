35b5a3aee7e2d97bc47b1b4be483f34ef3f064aa
SpuerMomonga <1638829841@qq.com>
2024-07-21 20:48:05 +0800

    feat: 增加系统托盘图标显示

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::tray::TrayIconBuilder;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,6 +12,14 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .setup(move |app| {
+            let handle = app.handle();
+            let _ = TrayIconBuilder::with_id("search")
+                .tooltip("Kit")
+                .icon(app.default_window_icon().unwrap().clone())
+                .build(app);
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
