20b1dcc03644456c181c3745fe8facc5e2212596
SpuerMomonga <1638829841@qq.com>
2024-07-22 11:00:01 +0800

    fix:  修改入口函数

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,15 @@
+
 mod tray;
 
-pub fn run() {}
-
-pub fn run_app() {}
+pub fn run_app() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .setup(move |app| {
+            let handle = app.handle();
+            tray::create_tray(handle)?;
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
