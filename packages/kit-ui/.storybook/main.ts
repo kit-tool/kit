de5f6da30efee578ffa2e6db898e694db8093f0f
SpuerMomonga <1638829841@qq.com>
2024-08-13 15:46:40 +0800

    build: 新增storybook工具

---
+++ packages/kit-ui/.storybook/main.ts
@@ -0,0 +1,22 @@
+import { dirname, join } from 'path';
+import type { StorybookConfig } from '@storybook/react-vite';
+
+function getAbsolutePath(value: string) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+  stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
+};
+export default config;
