5cdfe2bc2d3859017e2a3484a3f9b043de0265b3
SpuerMomonga <1638829841@qq.com>
2024-08-13 09:23:22 +0800

    build: 修改打包配置

--- packages/kit/vite.config.ts
+++ packages/kit/vite.config.ts
@@ -1,32 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 // import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 2024,
     strictPort: true,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/crates/**"],
+      ignored: ['**/crates/**'],
     },
   },
   css: {
     postcss: {
       // plugins: [tailwindcss]
-    }
-  }
+    },
+  },
 }));
