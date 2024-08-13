2be9485ad9324c7e9f6c8fef34800050230566c3
SpuerMomonga <1638829841@qq.com>
2024-08-13 23:57:16 +0800

    build: storybook 构建工具配置

--- packages/kit-ui/src/stories/Button.stories.ts
+++ packages/kit-ui/src/stories/Button.stories.ts
@@ -1,48 +1,57 @@
 import { fn } from '@storybook/test';
 import { Button } from './Button';
+import { Meta } from '@storybook/react';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-export default {
+const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    variant: 'default',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    onClick: { control: 'check', description: '按钮点击事件' },
+    size: {
+      control: { type: 'select' },
+      options: ['default', 'sm', 'lg', 'icon'],
+      description: '按钮大小',
+      table: { defaultValue: { summary: 'default' } },
+    },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  args: { onClick: fn(), size: 'default' },
 };
+
+export default meta;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
-    label: 'Button',
+    children: 'Button',
   },
 };
 
-export const Secondary = {
+export const Destructive = {
   args: {
-    label: 'Button',
+    variant: 'destructive',
+    children: 'Button',
   },
 };
 
-export const Large = {
+export const Sm = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: 'sm',
+    children: 'Button',
   },
 };
 
-export const Small = {
+export const Lg = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: 'lg',
+    children: 'Button',
   },
 };
